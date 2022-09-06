c0bb0942bb582b393ad59ced358037022c1eaace
caicodes <caicodes1@gmail.com>
2022-09-05 21:58:09 -0600

    updated fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import typography from "./utils/typography";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App typography={typography} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/utils/typography.js
+++
@@ -1,9 +0,0 @@
-import Typography from "typography";
-import grandViewTheme from "typography-theme-grand-view";
-import injectFonts from "typography-inject-fonts";
-
-const typography = new Typography(grandViewTheme);
-typography.injectStyles();
-injectFonts(typography);
-
-export default typography;
